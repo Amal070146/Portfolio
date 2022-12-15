b788ab42859adf4d6aa1035e4ff4c918bdfeef0f
Amal C P <acp070146@gmail.com>
2022-12-15 17:24:53 +0530

    some errors changed

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,7 @@ import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/Amal_C_P.png";
 import HeaderSocial from "./headersocial";
+import { BsFillFileArrowDownFill } from "react-icons/bs";
 
 const header = () => {
   return (
@@ -18,16 +19,18 @@ const header = () => {
           </div>
 
           <div className="me">
-            <div className="social_media_in">
+            {/* <div className="social_media_in">
               <HeaderSocial />
+            </div> */}
+            <div className="header_image">
+              <img src={ME} alt="me" />
             </div>
-
-            <img src={ME} alt="me" />
           </div>
-
-          <a href="#contact" className="scroll_down">
-            Scroll Down
-          </a>
+          <div>
+            <a href="#contact" className="scroll_down">
+              <BsFillFileArrowDownFill />
+            </a>
+          </div>
         </div>
       </div>
     </header>
