799d6e5d1943bd81766a756790fd400d15bb42e5
Amal C P <acp070146@gmail.com>
2023-01-11 07:33:26 +0530

    new update wit js format input data on portfolio page

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -7,51 +7,50 @@ import IMG4 from "../../assets/portfolio1.png";
 import IMG5 from "../../assets/portfolio1.png";
 import IMG6 from "../../assets/portfolio1.png";
 
-
 const data = [
   {
-    id : 1,
-    image:IMG1,
-    title: 'Lab Report Daily basis analysing application',
-    github: 'https://github.com/Amal070146/TEZER',
-    demo : 'https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900'
+    id: 1,
+    image: IMG1,
+    title: "Lab Report Daily basis analysing application",
+    github: "https://github.com/Amal070146/TEZER",
+    demo: "https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900",
   },
   {
-    id : 2,
-    image:IMG1,
-    title: 'Lab Report Daily basis analysing application',
-    github: 'https://github.com/Amal070146/TEZER',
-    demo : 'https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900'
+    id: 2,
+    image: IMG1,
+    title: "Lab Report Daily basis analysing application",
+    github: "https://github.com/Amal070146/TEZER",
+    demo: "https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900",
   },
   {
-    id : 3,
-    image:IMG1,
-    title: 'Lab Report Daily basis analysing application',
-    github: 'https://github.com/Amal070146/TEZER',
-    demo : 'https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900'
+    id: 3,
+    image: IMG1,
+    title: "Lab Report Daily basis analysing application",
+    github: "https://github.com/Amal070146/TEZER",
+    demo: "https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900",
   },
   {
-    id : 4,
-    image:IMG1,
-    title: 'Lab Report Daily basis analysing application',
-    github: 'https://github.com/Amal070146/TEZER',
-    demo : 'https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900'
+    id: 4,
+    image: IMG1,
+    title: "Lab Report Daily basis analysing application",
+    github: "https://github.com/Amal070146/TEZER",
+    demo: "https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900",
   },
   {
-    id : 5,
-    image:IMG1,
-    title: 'Lab Report Daily basis analysing application',
-    github: 'https://github.com/Amal070146/TEZER',
-    demo : 'https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900'
+    id: 5,
+    image: IMG1,
+    title: "Lab Report Daily basis analysing application",
+    github: "https://github.com/Amal070146/TEZER",
+    demo: "https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900",
   },
   {
-    id : 6,
-    image:IMG1,
-    title: 'Lab Report Daily basis analysing application',
-    github: 'https://github.com/Amal070146/TEZER',
-    demo : 'https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900'
+    id: 6,
+    image: IMG1,
+    title: "Lab Report Daily basis analysing application",
+    github: "https://github.com/Amal070146/TEZER",
+    demo: "https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900",
   },
-]
+];
 
 const portfolio = () => {
   return (
@@ -60,127 +59,24 @@ const portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio_container">
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta"><a
-            href="https://github.com/Amal070146"
-            className="btn"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a></div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta"><a
-            href="https://github.com/Amal070146"
-            className="btn"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a></div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta"><a
-            href="https://github.com/Amal070146"
-            className="btn"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a></div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta"><a
-            href="https://github.com/Amal070146"
-            className="btn"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a></div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta"><a
-            href="https://github.com/Amal070146"
-            className="btn"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a></div>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta"><a
-            href="https://github.com/Amal070146"
-            className="btn"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://cdn.dribbble.com/userupload/3864186/file/original-e76878961f17b4df6817b0d95a4a4d34.png?compress=1&resize=1200x900"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a></div>
-        </article>
-        
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio_item_image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item_cta">
+                <a href={github} className="btn" target="_blank">
+                  GitHub
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
