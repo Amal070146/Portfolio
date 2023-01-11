29913c2728fdf572bded23259834a2013c0ed025
Amal C P <acp070146@gmail.com>
2023-01-11 06:57:21 +0530

    updation experience

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from '../../assets/'
 
 const portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+      
 
       <div className="container portfolio_container">
         <article className="portfolio_item">
-          <div className="portfolio_item_image"></div>
+          <div className="portfolio_item_image">
+
+          </div>
           <h3>This is a portfolio item title</h3>
           <a href="https://github.com/Amal070146" className="btn" target='_blank'>
             GitHub
