e2f988c5c423c41af2fbd66819aea8183f9b27cf
Amal C P <acp070146@gmail.com>
2023-01-11 08:18:41 +0530

    section testimonial structuring is done

--- src/components/testimonials/testiomonials.jsx
+++ src/components/testimonials/testiomonials.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/amal.jpg";
+import AVTR2 from "../../assets/amal.jpg";
+import AVTR3 from "../../assets/amal.jpg";
+import AVTR4 from "../../assets/amal.jpg";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Some name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat magni nostrum expedita laudantium commodi molestiae deleniti officiis soluta. Voluptate minima temporibus quaerat excepturi natus repellendus corporis aperiam modi incidunt.",
+  },
+  {
+    avatar: AVTR1,
+    name: "Some name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat magni nostrum expedita laudantium commodi molestiae deleniti officiis soluta. Voluptate minima temporibus quaerat excepturi natus repellendus corporis aperiam modi incidunt.",
+  },
+  {
+    avatar: AVTR1,
+    name: "Some name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat magni nostrum expedita laudantium commodi molestiae deleniti officiis soluta. Voluptate minima temporibus quaerat excepturi natus repellendus corporis aperiam modi incidunt.",
+  },
+  {
+    avatar: AVTR1,
+    name: "Some name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat magni nostrum expedita laudantium commodi molestiae deleniti officiis soluta. Voluptate minima temporibus quaerat excepturi natus repellendus corporis aperiam modi incidunt.",
+  },
+];
 
 const testiomonials = () => {
   return (
-<section id='testiomonials'>Testimonials</section>  )
-}
+    <section id="testiomonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default testiomonials
+      <Swiper className="container testimonials_container" 
+       // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={3}
+      pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={avatar} alt="" />
+              </div>
+              <h5 className="client_name">{name}</h5>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default testiomonials;
