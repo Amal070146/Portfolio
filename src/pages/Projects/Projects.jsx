e35b79d4293962375aa40ee9012760277a61ebaa
Amal C P <acp070146@gmail.com>
2024-01-01 20:47:32 +0530

    project updation

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 import { AiOutlineHome } from "react-icons/ai";
 import { BiUserCircle } from "react-icons/bi";
@@ -16,77 +16,85 @@ import IMG6 from "../../assets/portfolio1.png";
 import IMG7 from "../../assets/projects/Cholaarts.png";
 import IMG8 from "../../assets/projects/gtaHackathon.png";
 import IMG9 from "../../assets/projects/sarahsTechno.png";
-
+import IMG10 from "../../assets/projects/opengrad.png";
 
 const Projects = () => {
-    const [activeNav, setActiveNav] = useState("#");
+  const [activeNav, setActiveNav] = useState("#");
 
-    const data = [
-      {
-        id: 1,
-        image: IMG1,
-        title: "CODe Design Week Website",
-        github: "https://github.com/amalcpaulson/code-design-week",
-        demo: "https://codedesignweek.netlify.app/",
-      },
-      {
-        id: 2,
-        image: IMG2,
-        title: "Website - Mulearn CCE Chapter",
-        github: "https://github.com/amalcpaulson/mulearn_cce",
-        demo: "https://mulearn-cce.vercel.app/",
-      },
-      {
-        id: 3,
-        image: IMG3,
-        title: "Web applicaation for Fundesign",
-        github: "https://github.com/amalcpaulson/fundesign",
-        demo: "https://fundesign.vercel.app/",
-      },
-      {
-        id: 4,
-        image: IMG4,
-        title: "WRAP - Waste Management Application",
-        github: "https://github.com/amalcpaulson/Wrap-Django",
-        demo: "https://wrap-web-app.netlify.app/",
-      },
-      {
-        id: 5,
-        image: IMG5,
-        title: "Unnathi - Website",
-        github: "https://github.com/amalcpaulson/unnathi-vite",
-        demo: "https://unnathi-new.vercel.app/",
-      },
+  const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: "CODe Design Week Website",
+      github: "https://github.com/amalcpaulson/code-design-week",
+      demo: "https://codedesignweek.netlify.app/",
+    },
+    {
+      id: 2,
+      image: IMG2,
+      title: "Website - Mulearn CCE Chapter",
+      github: "https://github.com/amalcpaulson/mulearn_cce",
+      demo: "https://mulearn-cce.vercel.app/",
+    },
+    {
+      id: 3,
+      image: IMG3,
+      title: "Web applicaation for Fundesign",
+      github: "https://github.com/amalcpaulson/fundesign",
+      demo: "https://fundesign.vercel.app/",
+    },
+    {
+      id: 4,
+      image: IMG4,
+      title: "WRAP - Waste Management Application",
+      github: "https://github.com/amalcpaulson/Wrap-Django",
+      demo: "https://wrap-web-app.netlify.app/",
+    },
+    {
+      id: 5,
+      image: IMG5,
+      title: "Unnathi - Website",
+      github: "https://github.com/amalcpaulson/unnathi-vite",
+      demo: "https://unnathi-new.vercel.app/",
+    },
 
-      {
-        id: 7,
-        image: IMG7,
-        title: "Chola Art Gallery",
-        github: "https://github.com/amalcpaulson/Chola-Arts-Gallery",
-        demo: "https://cholaart.in/",
-      },
-      {
-        id: 8,
-        image: IMG8,
-        title: "GTA Hackathon (Mulearn) - Website",
-        github: "https://github.com/amalcpaulson/gtahackathon",
-        demo: "https://gta.mulearn.org/",
-      },
-      {
-        id: 9,
-        image: IMG9,
-        title: "Sarah's Techno",
-        github: "https://github.com/amalcpaulson/SarasTechno",
-        demo: "https://saras-techno.vercel.app/",
-      },
-      {
-        id: 6,
-        image: IMG6,
-        title: "Lab Report Daily basis analysing application",
-        github: "https://github.com/amalcpaulson/TEZER",
-        demo: "https://github.com/amalcpaulson/TEZER",
-      },
-    ];
+    {
+      id: 7,
+      image: IMG7,
+      title: "Chola Art Gallery",
+      github: "https://github.com/amalcpaulson/Chola-Arts-Gallery",
+      demo: "https://cholaart.in/",
+    },
+    {
+      id: 8,
+      image: IMG8,
+      title: "GTA Hackathon (Mulearn) - Website",
+      github: "https://github.com/amalcpaulson/gtahackathon",
+      demo: "https://gta.mulearn.org/",
+    },
+    {
+      id: 10,
+      image: IMG10,
+      title: "OpenGrad",
+      github: "https://github.com/amalcpaulson/opengrad-frontend",
+      demo: "https://opengrads.netlify.app/",
+    },
+    {
+      id: 9,
+      image: IMG9,
+      title: "Sarah's Techno",
+      github: "https://github.com/amalcpaulson/SarasTechno",
+      demo: "https://saras-techno.vercel.app/",
+    },
+
+    {
+      id: 6,
+      image: IMG6,
+      title: "Lab Report Daily basis analysing application",
+      github: "https://github.com/amalcpaulson/TEZER",
+      demo: "https://github.com/amalcpaulson/TEZER",
+    },
+  ];
 
   return (
     <div>
@@ -131,7 +139,7 @@ const Projects = () => {
       </div>
       <div className="projects-wrapper">
         <div id="portfolio" className="portfolio-wrapper">
-          <div className="portfolio-header" style={{marginTop:"15px"}}>
+          <div className="portfolio-header" style={{ marginTop: "15px" }}>
             <h2>My Projects</h2>
           </div>
           <div className="portfolio_container">
@@ -155,12 +163,10 @@ const Projects = () => {
               );
             })}
           </div>
-
-         
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Projects
+export default Projects;
